a4076a8cf60d762e38f0ff37489339b4b2f16510
Maksyks <106544465+Maksyks@users.noreply.github.com>
2022-05-31 00:15:11 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node //создаем структуру с двумя переменными
+{
+	char key;
+	int size;
+	Node* next = NULL;
+	Node* right = NULL;
+	Node* left = NULL;
+};
+//Функция определяет частоты
+void Frequence()
+{
+	FILE* Start = NULL;
+	FILE* Result = NULL;
+	fopen_s(&Start, "Start.txt", "r");//открываем файл
+	unsigned int FrQ[256] = { 0 }; //пустая таблица частот
+	unsigned char s = 0;
+	//заполняем таблицу частот
+	if (Start)
+		while (fscanf_s(Start, "%c", &s) != -1)
+			FrQ[s]++;
+
+	/*fopen_s(&Result, "Result.txt", "w");//открываем файл
+	for (int i = 0;i < 256;i++)
+		fprintf(Result, "%d", FrQ[i]);*/
+
+}
+
+int main()
+{
+	Frequence();
+}
